bcba37c3515755a75f3520de6cc74e1180483423
El Jakani Yassine <59035087+Eljakani@users.noreply.github.com>
2023-02-11 15:51:43 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    autoplay:true,
+    items:4,
+    align :'center',
+    margin:10,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    stagePadding:30,
+    smartSpeed:450,
+    responsiveClass:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive : {
+      0 : {
+              items:1,
+              margin:2,
+      },
+      480 : {
+              items:2,
+              margin:5
+            },
+      768 : {
+              items:4,
+              margin:10
+      }
+    },
+});
+    
+});
