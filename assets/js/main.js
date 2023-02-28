62a3502dfdc2b2d3e5f1c4e1ba9040bf353c3f31
SAHTANI Mohcine <mohcine.sahtani@gmail.com>
2023-02-28 12:57:56 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,17 +13,27 @@ $(document).ready(function(){
     responsive : {
       0 : {
               items:1,
-              margin:0,
+              margin:100,
       },
       480 : {
               items:2,
-              margin:2,
+              margin:20,
                 autoplay:true,
             },
       768 : {
-              items:4,
+              items:3,
               margin:5,
-                autoplay:true,
+              autoplay:true,
+      },
+      900: {
+      		items: 3,
+      		margin: 40,
+      		autoplay: true
+      },
+      1200: {
+      	items: 4,
+      	margin: 10,
+      	autoplay: true
       }
     },
 });
